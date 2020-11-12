064cef842da220717a851cd685dfc3d29f168000
Helga777q <helga.qa72@gmail.com>
2020-11-12 08:21:20 +0200

    обновлен класс MyFirstSeleniumTest

--- src/test/java/MyFirstSeleniumTest.java
+++ src/test/java/MyFirstSeleniumTest.java
@@ -1,30 +1,40 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class MyFirstSeleniumTest {
+
  private WebDriver wd;
 
- @Test
-  public void openSoftTestingSiteChrome(){
-  wd = new ChromeDriver();
-  wd.get("https://software-testing.ru/");
-  assertEquals(wd.getTitle(), "Software-Testing.Ru");
+ @BeforeTest
+ public void startChrome(){
+  ChromeOptions options = new ChromeOptions();
+  options.addArguments("-lang=en-GB");
+  wd = new ChromeDriver(options);
  }
 
+
  @Test
- public void openGoogleFirefox(){
-  wd=new FirefoxDriver();
-  wd.get("https://google.com");
+ public void openGoogleAndSearch(){
+  wd.navigate().to("https://google.com"); //equal wd.get("link to address")
   assertEquals(wd.getTitle(), "Google");
+  WebElement element = wd.findElement(By.name("q"));
+  element.sendKeys("webdriver");
+  element.submit();
+  //wd.findElement(By.name("btnK")).click();
+  assertEquals(wd.getTitle(), "webdriver - Google Search");
+
  }
 
- @AfterMethod
+ @AfterTest
  public void browserQuit(){
   wd.quit();
- }
+}
 
 }
