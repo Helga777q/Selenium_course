61db9d2d8c1142bb4eeb62cf1e5e5102dab7d719
Helga777q <helga.qa72@gmail.com>
2020-11-23 19:56:21 +0200

    L4- Admin menu Test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,7 +20,7 @@ public class TestBase {
     wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
   }
 
-@AfterMethod
+  @AfterMethod
   public void stop(){
     wd.quit();
   }
@@ -32,6 +33,15 @@ public class TestBase {
     } catch (NoSuchElementException ex) {
       return false;
     }
+  }
+
+  //check if elements are present
+  boolean areElementsPresent(By locator) {
+    return wd.findElements(locator).size() > 0;
+  }
+
+  public void mainPage(){
+    wd.get("http://localhost/litecart/");
   }
 
 
