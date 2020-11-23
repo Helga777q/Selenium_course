26ad6c0cfc8a4dda73edf07bc53d14ad45b0389a
Helga777q <helga.qa72@gmail.com>
2020-11-23 19:56:41 +0200

    L4 - тест наличие дейблов у товаров

---
+++ src/test/java/tests/StickersCheckTests.java
@@ -0,0 +1,64 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class StickersCheckTests extends TestBase {
+
+  @Test
+  public void stickersPopularProductsTest() {
+    mainPage();
+    WebElement popularProducts = wd.findElement(By.id("box-popular-products"));
+    List<WebElement> products = popularProducts.findElements(By.className("product-column"));
+    assertTrue(assertStickersCount(products));
+
+  }
+
+  @Test
+  public void stickersCampaignProductsTest() {
+    mainPage();
+    WebElement campaign = wd.findElement(By.id("box-campaign-products"));
+    List<WebElement> products = campaign.findElements(By.className("product-column"));
+    assertTrue(assertStickersCount(products));
+
+  }
+
+  @Test
+  public void stickersLatestProductsTest() {
+    mainPage();
+    WebElement latestProducts = wd.findElement(By.id("box-latest-products"));
+    List<WebElement> products = latestProducts.findElements(By.className("product-column"));
+    assertTrue(assertStickersCount(products));
+  }
+
+
+  @Test
+  public void stickersAllProductsTest() {
+    mainPage();
+    List<WebElement> allProducts = wd.findElements(By.className("product-column"));
+    assertTrue(assertStickersCount(allProducts));
+  }
+
+
+  private boolean assertStickersCount(List<WebElement> products) {
+    ArrayList<Boolean> test = new ArrayList();
+    ArrayList<Boolean> test2 = new ArrayList();
+    for (WebElement product : products) {
+      List<WebElement> labels = product.findElements(By.cssSelector("div[class^='sticker'])"));
+      System.out.println(labels.size());
+      if (labels.size() == 1) {
+        test.add(true);
+      } else {
+        test2.add(false);
+      }
+    }
+    return products.size() == test.size();
+  }
+}
