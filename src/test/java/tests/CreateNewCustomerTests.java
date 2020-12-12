496746b53a0c930be4f97540289b98048acaf8da
Helga777q <helga.qa72@gmail.com>
2020-12-12 22:58:22 +0200

    L11 в классе CreateNewCustomerTests отделены тестовые данные

--- src/test/java/tests/CreateNewCustomerTests.java
+++ src/test/java/tests/CreateNewCustomerTests.java
@@ -1,23 +1,42 @@
 package tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import models.Customer;
-import org.apache.commons.text.RandomStringGenerator;
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class CreateNewCustomerTests extends TestBase{
 
+  @DataProvider
+  public Iterator<Object[]> customers() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/customers.json")));
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<Customer> customers = gson.fromJson(json, new TypeToken<List<Customer>>() {
+      }.getType());
+      return customers.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+  }
 
-  @Test
-  public void newCustomer() throws InterruptedException {
+
+
+
+  @Test (dataProvider = "customers")
+  public void registerNewCustomer(Customer customer)  {
     mainPage();
     initNewAccountCreation();
-    String email = generateEmail();
-    Customer customer = new Customer().withFirstName("Olga").withLastName("Test2").withAddress("Some street").withPostcode("12345")
-            .withCity("Odessa").withCountry("United States").withZone("Delaware")
-            .withEmail(email).withPhone("+1667777").withPassword("12345678");
     fillNewAccountForm(customer);
     submitForm();
     logout();
@@ -25,7 +44,7 @@ public class CreateNewCustomerTests extends TestBase{
     logout();
   }
 
-  private void fillNewAccountForm(Customer customer) throws InterruptedException {
+  private void fillNewAccountForm(Customer customer)  {
     type(By.name("firstname"), customer.getFirstName());
     type(By.name("lastname"), customer.getLastName());
     type(By.name("address1"), customer.getAddress());
@@ -40,11 +59,11 @@ public class CreateNewCustomerTests extends TestBase{
 
   }
 
-  private void submitForm() throws InterruptedException {
+  private void submitForm()  {
     click(By.cssSelector("button[name='create_account']"));
   }
 
-  private void login(Customer customer) throws InterruptedException {
+  private void login(Customer customer)  {
     type(By.cssSelector("input[name='email']"), customer.getEmail());
     type(By.cssSelector("input[name='password']"), customer.getPassword());
     click(By.cssSelector("button[name='login']"));
@@ -54,19 +73,14 @@ public class CreateNewCustomerTests extends TestBase{
 
 
 
-  private void logout() throws InterruptedException {
+  private void logout()  {
     click(By.linkText("Logout"));
   }
 
-  private void initNewAccountCreation() throws InterruptedException {
+  private void initNewAccountCreation()  {
     click(By.cssSelector("form[name='login_form'] > table a"));
   }
 
-
-  private String generateEmail(){
-    return new RandomStringGenerator.Builder()
-            .withinRange('a', 'z').build().generate(7)+"@example.com";
-  }
 
 
 }
